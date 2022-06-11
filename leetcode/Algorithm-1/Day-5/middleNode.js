d3daa1185a369798a885d2e4e648c69a77daedb8
Le Van Cong <lecongdtvt10@gmail.com>
2022-06-11 14:19:54 +0900

    Runtime: 77 ms, faster than 54.05% of JavaScript online submissions for Middle of the Linked List.

---
+++ leetcode/Algorithm-1/Day-5/middleNode.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var middleNode = function(head) {
+    let len =  0;
+    let node = head;
+    while(node !== null){
+        len += 1;
+        node =  node.next;
+    }
+    let middle = Math.floor(len/2);
+    node = head;
+    for (let i = 0; i< middle; i++){
+        node = node.next;
+    }
+    // console.log(node)
+    return node;
+    
+};
