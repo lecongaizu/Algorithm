1eadd364ebce3de16a84cc469447222198444529
Le Van Cong <lecongdtvt10@gmail.com>
2022-06-20 15:34:30 +0900

    Runtime: 143 ms, faster than 5.16% of JavaScript online submissions for Number of 1 Bits.

---
+++ leetcode/Algorithm-1/Day-13/hammingWeight.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+    let s = n.toString(2)
+
+    let count = 0
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == 1) {
+            count++
+        }
+    }
+
+    return count
+};
